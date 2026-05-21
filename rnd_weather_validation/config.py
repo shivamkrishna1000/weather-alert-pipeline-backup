36626f10d46af33ace49b5994574558459b455b7
Shivam Krishna <50731434+shivamkrishna1000@users.noreply.github.com>
2026-05-21 20:05:19 +0530

    rnd config file updated

--- rnd_weather_validation/config.py
+++ rnd_weather_validation/config.py
@@ -3,18 +3,14 @@ LOCATION_CONFIGS = [
         "name": "RnD",
         "latitude": 17.02818,
         "longitude": 78.14797,
-        "recipients": [
-            # "918605102018",
-            "918709737257"
-            # "919966970290",
-        ],
+        "recipients": ["918605102018", "919966970290", "918709737257"],
     },
-    {
-        "name": "Patna",
-        "latitude": 25.609695,
-        "longitude": 85.194362,
-        "recipients": [
-            "918709737257",
-        ],
-    },
+    # {
+    #    "name": "Patna",
+    #    "latitude": 25.609695,
+    #    "longitude": 85.194362,
+    #    "recipients": [
+    #        "918709737257",
+    #    ],
+    # },
 ]
